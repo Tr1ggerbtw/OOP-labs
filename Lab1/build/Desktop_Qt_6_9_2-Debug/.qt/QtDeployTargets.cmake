ad07cd9c8db972e12944d908705d3012a8b1bf35
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-09-14 18:42:04 +0300

    removed build folder

--- Lab1/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Lab1_FILE /home/tr1ggerbtw/Projects/oop/Lab1/build/Desktop_Qt_6_9_2-Debug/Lab1)
-set(__QT_DEPLOY_TARGET_Lab1_TYPE EXECUTABLE)
--- Lab1/build/Desktop_Qt_6_9_2-Debug/.qt/deploy_Lab1_cee4170dd5.cmake
+++
@@ -1,8 +0,0 @@
-include("/home/tr1ggerbtw/Projects/oop/Lab1/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Lab1-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "/home/tr1ggerbtw/Projects/oop/Lab1/build/Desktop_Qt_6_9_2-Debug/Lab1"
-    GENERATE_QT_CONF
-)
